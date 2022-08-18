33e3f4c2c519202c330cb2b9cd1e7515660d79d4
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-19 00:40:22 +0600

    Image Changes

--- src/jsx/layouts/nav/NavHader.js
+++ src/jsx/layouts/nav/NavHader.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { ThemeContext } from "../../../context/ThemeContext";
 // Import Image
 import brand from "../../../images/logo-text.png";
+import logo from "../../../images/logo.png";
 
 const NavHader = () => {
   const [toggle, setToggle] = useState(false);
@@ -14,54 +15,8 @@ const NavHader = () => {
       <Link to="/dashboard" className="brand-logo">
         {background.value === "dark" || navigationHader !== "color_1" ? (
           <Fragment>
-            4
-            <div
-              className="logo-abbr"
-              width="80"
-              height="80"
-              viewBox="0 0 80 80"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g clip-path="url(#clip0)">
-                <rect
-                  className="rect-primary-rect"
-                  width="80"
-                  height="80"
-                  rx="16"
-                  fill="#1362FC"
-                />
-                <circle cx="42" cy="19" r="10" fill="white" />
-                <circle cx="75.5" cy="76.5" r="16.5" fill="#12A7FB" />
-                <circle cx="5.5" cy="1.5" r="17.5" fill="#1362FC" />
-                <circle
-                  className="rect-primary-rect-1"
-                  cx="5.5"
-                  cy="1.5"
-                  r="16.5"
-                  stroke="white"
-                  stroke-opacity="0.66"
-                  stroke-width="2"
-                />
-                <path
-                  d="M33.7656 87.2159C34.9565 76.5246 37.5874 53.6112 38.5845 47.4881V47.4881C39.1698 43.8941 40.2547 47.2322 39.8692 50.8531C38.9933 59.0813 37.1429 74.1221 35.5121 87.4131C33.1225 106.889 33.3507 95.974 33.7635 88.0818"
-                  stroke="white"
-                  stroke-width="21"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0">
-                  <rect
-                    className="rect-primary-rect"
-                    width="80"
-                    height="80"
-                    rx="16"
-                    fill="white"
-                  />
-                </clipPath>
-              </defs>
+            <div className="logo-abbr" width="80" height="80">
+              <img src={logo} alt="" />
             </div>
             <div className="brand-title" width="60" height="68">
               <img src={brand} alt="" />
@@ -94,13 +49,13 @@ const NavHader = () => {
                   cy="1.5"
                   r="16.5"
                   stroke="white"
-                  stroke-opacity="0.66"
-                  stroke-width="2"
+                  strokeOpacity="0.66"
+                  strokeWidth="2"
                 />
                 <path
                   d="M33.7656 87.2159C34.9565 76.5246 37.5874 53.6112 38.5845 47.4881V47.4881C39.1698 43.8941 40.2547 47.2322 39.8692 50.8531C38.9933 59.0813 37.1429 74.1221 35.5121 87.4131C33.1225 106.889 33.3507 95.974 33.7635 88.0818"
                   stroke="white"
-                  stroke-width="21"
+                  strokeWidth="21"
                   stroke-linecap="round"
                   stroke-linejoin="round"
                 />
--- src/jsx/pages/Login.js
+++ src/jsx/pages/Login.js
@@ -47,8 +47,8 @@ function Login (props) {
 							<circle cx="42" cy="19" r="10" fill="white"/>
 							<circle cx="75.5" cy="76.5" r="16.5" fill="#12A7FB"/>
 							<circle cx="5.5" cy="1.5" r="17.5" fill="#1362FC"/>
-							<circle className="rect-primary-rect-1" cx="5.5" cy="1.5" r="16.5" stroke="white" stroke-opacity="0.66" stroke-width="2"/>
-							<path d="M33.7656 87.2159C34.9565 76.5246 37.5874 53.6112 38.5845 47.4881V47.4881C39.1698 43.8941 40.2547 47.2322 39.8692 50.8531C38.9933 59.0813 37.1429 74.1221 35.5121 87.4131C33.1225 106.889 33.3507 95.974 33.7635 88.0818" stroke="white" stroke-width="21" stroke-linecap="round" stroke-linejoin="round"/>
+							<circle className="rect-primary-rect-1" cx="5.5" cy="1.5" r="16.5" stroke="white" stroke-opacity="0.66" strokeWidth="2"/>
+							<path d="M33.7656 87.2159C34.9565 76.5246 37.5874 53.6112 38.5845 47.4881V47.4881C39.1698 43.8941 40.2547 47.2322 39.8692 50.8531C38.9933 59.0813 37.1429 74.1221 35.5121 87.4131C33.1225 106.889 33.3507 95.974 33.7635 88.0818" stroke="white" strokeWidth="21" stroke-linecap="round" stroke-linejoin="round"/>
 							</g>
 							<defs>
 							<clipPath id="clip0">
