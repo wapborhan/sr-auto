936844558da34b2468fbab670ab0a0b25f65c65e
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-23 22:12:33 +0600

    Changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store} from './store/store';
+import reportWebVitals from "./reportWebVitals";
+import SimpleReactLightbox from "simple-react-lightbox";
+import  ThemeContext  from "./context/ThemeContext"; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store = {store}>
+            <SimpleReactLightbox>
+                <BrowserRouter basename='/react/demo'>
+                    <ThemeContext>
+                        <App />
+                    </ThemeContext>  
+                </BrowserRouter>    
+            </SimpleReactLightbox>
+        </Provider>	
+	</React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
